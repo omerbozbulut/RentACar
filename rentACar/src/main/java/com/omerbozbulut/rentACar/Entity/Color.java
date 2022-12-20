b08d64ed2ff1bdacae577f54c7319e5617657b51
omer <omerbzblt613@gmail.com>
2022-12-20 22:08:11 +0300

    color entity

---
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Entity/Color.java
@@ -0,0 +1,26 @@
+package com.omerbozbulut.rentACar.Entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="color")
+public class Color {
+
+    @Id
+    @GeneratedValue
+    private int colorID;
+
+    @NotNull
+    private String colorName;
+}
