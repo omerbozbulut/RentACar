23bb30a3aa696899723fd2c742b2e2b06a8769bf
omer <omerbzblt613@gmail.com>
2022-12-20 22:08:56 +0300

    fuel type entity

---
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Entity/FuelType.java
@@ -0,0 +1,28 @@
+package com.omerbozbulut.rentACar.Entity;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="fuelType")
+public class FuelType {
+
+    @Id
+    @GeneratedValue
+    private int fuelTypeID;
+
+    @NotNull
+    private String nameOfFuelType;
+
+}
