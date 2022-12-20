6b94ba628b0c737a3040beb2e4ecd039f30aedd3
omer <omerbzblt613@gmail.com>
2022-12-20 22:08:43 +0300

    province entity

---
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Entity/Province.java
@@ -0,0 +1,28 @@
+package com.omerbozbulut.rentACar.Entity;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="province")
+public class Province {
+
+    @Id
+    @GeneratedValue
+    private int provinceID;
+
+    @NotNull
+    private String provinceName;
+
+    @OneToMany(mappedBy = "province")
+    private List<Station> stationList;
+}
