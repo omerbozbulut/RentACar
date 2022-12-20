a06a4fbe9990be8789c1d96551fd3cff829368c6
omer <omerbzblt613@gmail.com>
2022-12-20 22:08:28 +0300

    driving license entity

---
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Entity/DrivingLicense.java
@@ -0,0 +1,27 @@
+package com.omerbozbulut.rentACar.Entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="drivingLicense")
+public class DrivingLicense {
+
+    @Id
+    @GeneratedValue
+    private int drivingLicenseID;
+
+    @NotNull
+    private String drivingLicenseName;
+
+    @OneToMany(mappedBy = "drivingLicense")
+    private List<Customer> customerList;
+}
