7ecdb7b55c349b6d9afe5ee582d02f52e06d9221
omer <omerbzblt613@gmail.com>
2022-12-28 02:50:36 +0300

    project main files

--- rentACarFrontEnd/veritabani/react-veritabani/src/App.js
+++ rentACarFrontEnd/veritabani/react-veritabani/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar.js";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Cars from "./Components/Cars.js";
+import SignIn from "./Components/SignIn.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Navbar />
+        <Route path="/cars" component={Cars} />
+        <Route path="/" component={SignIn} />
+      </Routes>
+    </Router>
   );
 }
 
