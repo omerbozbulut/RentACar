06c33e5343b07b5e0d85b4d31f58212fd19fa060
omer <omerbzblt613@gmail.com>
2023-01-08 19:02:01 +0300

    search car post mapping

--- rentACar/src/main/java/com/omerbozbulut/rentACar/Controller/CarController.java
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Controller/CarController.java
@@ -1,10 +1,14 @@
 package com.omerbozbulut.rentACar.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.omerbozbulut.rentACar.Entity.Car;
 import com.omerbozbulut.rentACar.Service.CarService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+
+import java.rmi.ServerException;
 import java.util.List;
 import java.util.Objects;
 import static org.springframework.http.HttpStatus.OK;
@@ -35,9 +39,16 @@ public class CarController {
 
     @GetMapping("/searchcar")
     public ResponseEntity<List<Car>> searchCar(@RequestParam() String name){
-        if (Objects.equals(name, "")){
-            return new ResponseEntity<>(carService.getAllCars(), OK);
-        }
         return new ResponseEntity<>(carService.searchCar(name), OK);
+    }
+
+    @PostMapping("/createCar")
+    public ResponseEntity<Car> createCar(@RequestBody Car car) throws ServerException {
+        int resultCar = carService.createCar(car);
+        if (resultCar == 0) {
+            throw new ServerException("NOT FOUND CAR");
+        } else {
+            return new ResponseEntity<>(car, HttpStatus.CREATED);
+        }
     }
 }
