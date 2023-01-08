ca7c5c7287420ebe66fd7b5116f4099dba6902bd
omer <omerbzblt613@gmail.com>
2023-01-08 19:03:13 +0300

    search gearboxtype

---
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Repository/GearboxTypeDao.java
@@ -0,0 +1,11 @@
+package com.omerbozbulut.rentACar.Repository;
+
+import com.omerbozbulut.rentACar.Entity.gearboxType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface GearboxTypeDao extends JpaRepository<gearboxType, Integer> {
+    @Query(name = "SELECT * FROM gearboxType WHERE gearboxTypeName = ?1", nativeQuery = true)
+    gearboxType getByGearboxTypeName(@Param("gearboxTypeName") String gearboxTypeName);
+}
