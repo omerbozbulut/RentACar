d7738f3ad81e7eaf9aecd2606fde3800e4a67042
omer <omerbzblt613@gmail.com>
2023-01-08 19:03:05 +0300

    search fueltype

---
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Repository/FuelTypeDao.java
@@ -0,0 +1,11 @@
+package com.omerbozbulut.rentACar.Repository;
+
+import com.omerbozbulut.rentACar.Entity.FuelType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface FuelTypeDao extends JpaRepository<FuelType,Integer> {
+    @Query(name = "SELECT * FROM fuelType WHERE fuelTypeName = ?1", nativeQuery = true)
+    FuelType getByFuelTypeName(@Param("fuelTypeName") String fuelTypeName);
+}
