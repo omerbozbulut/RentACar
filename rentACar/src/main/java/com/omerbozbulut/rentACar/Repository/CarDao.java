7f681b88b167f68a9fcae55909fa6338548f31e5
omer <omerbzblt613@gmail.com>
2023-01-08 19:02:59 +0300

    save and search car

--- rentACar/src/main/java/com/omerbozbulut/rentACar/Repository/CarDao.java
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Repository/CarDao.java
@@ -2,9 +2,12 @@ package com.omerbozbulut.rentACar.Repository;
 
 import com.omerbozbulut.rentACar.Entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 public interface CarDao extends JpaRepository<Car, Integer> {
@@ -18,7 +21,12 @@ public interface CarDao extends JpaRepository<Car, Integer> {
     @Query(value = "SELECT * From car ORDER BY price DESC", nativeQuery = true)
     List<Car> getHighToLowCars();
 
-    @Query(value = "Select * FROM car WHERE modelID IN (SELECT modelID FROM model WHERE model.modelName LIKE %?1%)", nativeQuery = true)
-    List<Car> searchCar(@Param("name") String name);
 
+    @Query(value = "{CALL carSearch(:kriter)}", nativeQuery = true)
+    List<Car> searchCar(@Param("kriter") String kriter);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO car VALUES(?1, ?2, ?4, ?5, ?8, ?6, ?9, ?7, ?3)", nativeQuery = true)
+    int saveCar(@Param("numberPlate") String numberPlate, @Param("vehicleClassID") int vehicleClassID, @Param("modelID") int modelID, @Param("gearboxTypeID") int gearboxTypeID, @Param("fuelTypeID") int fuelTypeID , @Param("colorID") int colorID, @Param("price") Float price, @Param("stationID") int stationID, @Param("productionDate") Date productionDate);
 }
