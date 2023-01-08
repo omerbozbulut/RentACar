879f3c10474f6e5d1758c000a270914b53987a37
omer <omerbzblt613@gmail.com>
2023-01-08 19:02:40 +0300

    search color

---
+++ rentACar/src/main/java/com/omerbozbulut/rentACar/Repository/ColorDao.java
@@ -0,0 +1,11 @@
+package com.omerbozbulut.rentACar.Repository;
+
+import com.omerbozbulut.rentACar.Entity.Color;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ColorDao extends JpaRepository<Color,Integer> {
+    @Query(name = "SELECT * FROM color WHERE colorName = ?1", nativeQuery = true)
+    Color getByColorName(@Param("colorName") String colorName);
+}
